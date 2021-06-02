a2e1d93daa9c78f5cec7bdf2bac6bb265dfb7000
arash <arash.asnaashari@gmail.com>
2021-06-02 22:09:56 +0430

    ahjaaa

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", express.static(path.join(__dirname, "public"))) ;
 
 app.listen(4000, () => console.log("start client!"));
